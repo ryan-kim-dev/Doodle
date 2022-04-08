b4543dca602487b2206092276151561f06cbb8fa
ryan-kim-dev <ryankim.h.dev@gmail.com>
2022-04-08 14:18:39 +0900

    implement javascript code

--- todo/main.js
+++ todo/main.js
@@ -1,6 +1,33 @@
+// const $form = document.querySelector('#todo-form');
+// console.log($form);
+
+// const items = document.querySelectorAll('.item');
+// items.forEach((tag) => {
+//   console.log(tag);
+// });
+// console.log(items);
+
+// todos.lastElementChild.remove();
+// todos.firstElementChild.textContent = '글씨 바꿨다!';
+// todos.lastElementChild.innerHTML = '<h1>태그도 바꿀 수 있다!</h1>';
+
 const $button = document.querySelector('.submit');
-$button.addEventListener('click', function (event) {
-  event.preventDefault();
-  console.log(event.target);
-  event.target.style.background = 'red';
-});
+const todos = document.querySelector('#todo-list');
+const todoInput = document.querySelector('#todo-input');
+const msg = document.querySelector('#msg');
+const onClickButton = (e) => {
+  e.preventDefault();
+  if (!todoInput.value) {
+    msg.style.display = 'block';
+    setTimeout(() => (msg.style.display = 'none'), 1000);
+    return;
+  }
+  const li = document.createElement('li'); // li태그를 생성하는 li변수 선언
+  li.appendChild(document.createTextNode(todoInput.value)); // li 요소 안에 텍스트 추가(node객체로)
+  li.classList.add('item'); // 생성한 li태그에 item 클래스를 부여하여 item 클래스에 부여한 css 스타일링 적용
+  todos.appendChild(li); // 자식요소 추가
+  todoInput.value = '';
+  todoInput.focus();
+};
+
+$button.addEventListener('click', onClickButton);
